79d363d4ecc829bf068044ad4c5b3251ca77cf52
OpeningStory <story@live.cn>
2025-02-26 13:18:28 +0800

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,21 @@
+<footer class="large-padding-top large-padding-bottom">
+  <div class="container-small">
+    <div class="row">
+      <div class="col-100 footer-bottom">
+        <div class="avatar"></div>
+        <a href="<?php echo BASE_URL; ?>/colophon.php" class="text-small text-color-secondary">跋</a>
+        <span>&sdot;</span>
+        <a href="https://dribbble.com/" class="text-small text-color-secondary">Dribbble</a>
+      </div>
+    </div>
+  </div>
+</footer>
+
+</body>
+
+<script src="<?php echo BASE_URL; ?>/assets/js/jquery.js"></script>
+<script src="<?php echo BASE_URL; ?>/assets/js/jquery.lazy.min.js"></script>
+<script src="<?php echo BASE_URL; ?>/assets/js/main.js" ></script>
+
+</body>
+</html>
---
+++ work.php
@@ -0,0 +1,21 @@
+<?php $page = "work";?>
+<?php include "header.php";?>
+
+<main>
+
+  <section class="hero">
+    <div class="container-small">
+      <div class="row">
+        <div class="col-100">
+          <h1>Selected projects <br><span class="text-color-secondary">2016-<?php echo date("y"); ?></span></h1>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section id="projects" class="projects-feed" >
+  </section>
+
+</main>
+
+<?php include "footer.php";?>
